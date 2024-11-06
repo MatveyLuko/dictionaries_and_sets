791ac0fe0ae8ae883d39a86baa5b7c01ead956c1
RealLowlyBlug <prikoldes228@mail.ru>
2024-11-07 00:37:46 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+#Работа со словарями:
+my_dict = {'Matvey': 2004, 'Kir': 2003, 'Kriko': 2006, 'Igor': 2009 }
+print(my_dict)
+print(my_dict['Matvey'])
+my_dict.update({'Grisha': 1982,
+                 'Anna': 1983})
+print(my_dict)
+del my_dict['Kriko']
+print(my_dict.get('Kriko'))
+
+#Работа с множествами:
+my_set = {1,2,3,2,1,1, 'book', 'string', 'book', True,}
+print(my_set)
+my_set.update(["cake",(1,2,3)])
+my_set.discard(2)
+print(my_set)
